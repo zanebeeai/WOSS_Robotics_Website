c23fb7e1b1a1c066adb940df85f6898393746cdd
zanzilla22 <69683242+zanzilla22@users.noreply.github.com>
2024-02-02 00:44:16 -0500

    Update _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -14,6 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0"
         />
+        <meta property="og:title" content="WOSS Robotics" />
+      <meta property="og:description" content="VEX 2106 & EV 843 @ White Oaks Secondary School" />
+      // <meta property="og:image" content="URL_to_your_image" />
       </Head>
       <Component {...pageProps} />
       <Analytics />
